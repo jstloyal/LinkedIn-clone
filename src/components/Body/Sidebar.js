8a0b4820c88b1326417bf96ca835d97b8a30fd01
jstloyal <jstloyalty@gmail.com>
2021-01-01 23:08:44 +0100

    add styling to sidebar

--- src/components/Body/Sidebar.js
+++ src/components/Body/Sidebar.js
@@ -4,10 +4,21 @@ import React from 'react';
 import './Sidebar.css';
 
 function Sidebar() {
+
+  const recentItems = (topic) => (
+    <div className="recentItem">
+      <span className="sidebar_hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
   return (
     <div className="sidebar">
       <div className="sidebar_top">
-        <img src="" alt="" />
+        <img 
+          src="https://images.unsplash.com/photo-1560345573-9f453083c335?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NjR8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60"
+          alt="cover pics" 
+        />
         <Avatar className="sidebar_avatar" />
         <h2>Adetayo Sunkanmi</h2>
         <h4>sunkanmi@mail.com</h4>
@@ -25,6 +36,10 @@ function Sidebar() {
 
       <div className="sidebar_bottom">
         <p>Recent</p>
+        {recentItems("reactjs")}
+        {recentItems('JavaScript')}
+        {recentItems('Ruby')}
+        {recentItems('Ruby on Rails')}
       </div>
     </div>
   )
