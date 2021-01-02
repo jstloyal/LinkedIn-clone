88c06d514e4a810e0427bb7aef9d9640be9c06db
jstloyal <jstloyalty@gmail.com>
2021-01-02 13:58:04 +0100

    connect app to firebase and add realtime posts

--- src/components/Body/Feed/Feed.js
+++ src/components/Body/Feed/Feed.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Feed.css';
 import CreateIcon from '@material-ui/icons/Create';
 import ImageIcon from '@material-ui/icons/Image';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
-// import EventNoteIcon from '@material-ui/icons/EventNote';
-// import CalenderViewDayIcon from '@material-ui/icons/CalenderViewDay';
 import InputOption from './InputOption';
 import Post from '../Post/Post';
+import { db } from '../../../firebase';
+import firebase from 'firebase';
 
 function Feed() {
+  const [input, setInput] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot(snapshot => (
+      setPosts(snapshot.docs.map(doc => (
+        {
+          id: doc.id,
+          data: doc.data(),
+        }
+      )))
+    ))
+  }, [])
+  
+  const sendPost = e => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      name: 'Adetayo Sunkanmi',
+      description: 'This is a test',
+      message: input,
+      photoUrl: '',
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput("");
+  };
+
   return (
     <div className="feed">
       <div className="feed_inputContainer">
         <div className="feed_input">
           <CreateIcon />
           <form>
-            <input type="text" />
-            <button type="submit">Send</button>
+            <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+            <button onClick={sendPost} type="submit">Send</button>
           </form>
         </div>
         <div className="feed_inputOption">
@@ -28,11 +55,16 @@ function Feed() {
           <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E" />
         </div>
       </div>
-      <Post
-        name="Adetayo Sunkanmi" 
-        description="This is a test" 
-        message="Wow this worked"
-      />
+
+      {posts.map(({ id, data: { name, description, message, photoUrl}}) => (
+        <Post 
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
+      ))}
     </div>
   );
 };
--- src/components/Body/Post/Post.js
+++ src/components/Body/Post/Post.js
@@ -1,18 +1,29 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
+import InputOption from '../Feed/InputOption';
 import './Post.css';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
 
 function Post({ name, description, message, photoUrl }) {
   return <div className="post">
       <div className="post_header">
         <Avatar />
         <div className="postInfo">
-          <h2>Adetayo Sunkanmi</h2>
-          <p>Description</p>
+          <h2>{name}</h2>
+          <p>{description}</p>
         </div>
       </div>
       <div className="post_body">
-        <p>Message goes here</p>
+        <p>{message}</p>
+      </div>
+      <div className="post_buttons">
+        <InputOption Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray" />
+        <InputOption Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+        <InputOption Icon={ShareOutlinedIcon} title="Share" color="gray" />
+        <InputOption Icon={SendOutlinedIcon} title="Send" color="gray" />
       </div>
   </div>;
 };
